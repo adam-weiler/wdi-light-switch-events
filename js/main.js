ace3d307524d2bee65c56f5209ad698717132ea5
adam-weiler <adam.weiler@yahoo.ca>
2019-08-20 14:06:22 -0400

    Light switch class is turning off and on when onClick.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const status = document.querySelector('.status');
+    status.innerText = 'The document is ready!'
 
+    const btnSwitch = document.querySelector('.switch');
+
+    // console.log(btnSwitch);
+
+    btnSwitch.addEventListener('click', (e) => {
+        // console.log(e);
+
+        let lightsOn = btnSwitch.className;
+        // console.log(lightsOn)
+
+        if (lightsOn == 'switch on'){
+            btnSwitch.className = 'switch off';
+            // console.log('Turn it off!')
+        } else if (lightsOn == 'switch off') {
+            btnSwitch.className = 'switch on';
+            // console.log('Turn it on!')
+        }
+
+  })
+
+})
 
